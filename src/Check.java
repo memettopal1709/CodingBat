fef5f1e2f45ec307b7df2906edf1a47a194c416a
memettopal1709 <memettopal1709@gmail.com>
2021-05-23 17:53:42 -0400

    CodingBat at Work Initial Commit

---
+++ src/Check.java
@@ -0,0 +1,13 @@
+public class Check {
+
+    public static void main(String[] args) {
+        String str = "edited";
+
+
+
+        System.out.println(str.substring(str.length()-2));
+
+    }
+}
+
+
